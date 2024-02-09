24f7cc615d910ef0ce554b9cbc74c7f775b4d76c
hwinsby <116369736+hwinsby@users.noreply.github.com>
2024-02-09 10:21:27 -0700

    fix: hover styles not applying on build; added ease-in-out

--- frontend/src/components/cups/Cups.tsx
+++ frontend/src/components/cups/Cups.tsx
@@ -1,18 +1,24 @@
 import { ReactEventHandler, useState } from "react";
 //import { useWebSocket } from "../hooks/useWebsocket";
-import redCup from "../../images/redCup.png";
-import greenCup from "../../images/greenCup.png";
-import yellowCup from "../../images/yellowCup.png";
+import redCupImg from "../../images/redCup.png";
+import greenCupImg from "../../images/greenCup.png";
+import yellowCupImg from "../../images/yellowCup.png";
 
 export const Cups = () => {
   //const { send } = useWebSocket();
   //console.log("send", send);
 
-  //   const redCup = { img: redCupImg, color: "red" };
-  //   const yellowCup = { img: yellowCupImg, color: "yellow" };
-  //   const greenCup = { img: greenCupImg, color: "green" };
+  enum CupColorEnum {
+    RED = "red",
+    YELLOW = "yellow",
+    GREEN = "green",
+  }
 
-  type CupColor = typeof redCup | typeof yellowCup | typeof greenCup;
+  const redCup = { img: redCupImg, color: CupColorEnum.RED };
+  const yellowCup = { img: yellowCupImg, color: CupColorEnum.YELLOW };
+  const greenCup = { img: greenCupImg, color: CupColorEnum.GREEN };
+
+  type CupColor = { img: string; color: CupColorEnum };
 
   type SelectedCup = typeof redCup | typeof yellowCup | typeof greenCup;
   const [selectedCup, setSelectedCup] = useState<CupColor>(greenCup);
@@ -41,15 +47,15 @@ export const Cups = () => {
   //   };
 
   const getNextCup = (newSelectedCup: SelectedCup) => {
-    if (newSelectedCup === greenCup) {
+    if (newSelectedCup.color === CupColorEnum.GREEN) {
       setSideCup1(yellowCup);
       setSideCup2(redCup);
     }
-    if (newSelectedCup === yellowCup) {
+    if (newSelectedCup.color === CupColorEnum.YELLOW) {
       setSideCup1(greenCup);
       setSideCup2(redCup);
     }
-    if (newSelectedCup === redCup) {
+    if (newSelectedCup.color === CupColorEnum.RED) {
       setSideCup1(greenCup);
       setSideCup2(yellowCup);
     }
@@ -57,10 +63,10 @@ export const Cups = () => {
 
   return (
     <div className="flex-auto h-1/3 flex gap-4 mb-10">
-      <Cup cup={selectedCup} selectedCup={true} />
+      <Cup cup={selectedCup.img} selectedCup={true} />
       <div className=" flex-auto w-1/3 h-1/2 justify-center self-end flex">
         <Cup
-          cup={sideCup1}
+          cup={sideCup1.img}
           selectedCup={false}
           onClick={() => {
             setSelectedCup(sideCup1);
@@ -68,7 +74,7 @@ export const Cups = () => {
           }}
         />
         <Cup
-          cup={sideCup2}
+          cup={sideCup2.img}
           selectedCup={false}
           onClick={() => {
             setSelectedCup(sideCup2);
@@ -95,14 +101,18 @@ const Cup = ({
 }) => {
   const [isHovered, setIsHovered] = useState(false);
 
-  const containerClassName = selectedCup
-    ? "relative flex justify-center h-full flex-auto w-1/3  flex-col items-center"
-    : "relative flex justify-center h-full";
-  const cupClassName = selectedCup ? "rotate-180 h-full" : "rotate-180 h-full hover:-translate-y-2";
-  const shadowClassName = selectedCup
-    ? "absolute -mb-10 w-[83%] h-9 bg-[#252931] rounded-[100%] -bottom-2 z-0 blur transform scale-75"
-    : "absolute -mb-9 w-full h-7 bg-[#252931] rounded-[100%] -bottom-2 z-0 blur transform scale-75" +
-      (isHovered ? " w-[85%] bg-[#252931D9]" : "");
+  const containerClassName = `relative flex justify-center h-full ${
+    selectedCup && "flex-auto w-1/3 flex-col items-center"
+  }`;
+  const cupClassName = `transform rotate-180 h-full transition ease-in-out duration-150 ${
+    !selectedCup && "hover:-translate-y-2 cursor-pointer"
+  }`;
+  const selectedShadow =
+    "absolute -mb-10 h-9 w-full transform scale-75 bg-[#252931] rounded-[100%] -bottom-2 z-0 blur";
+  const sideShadow = `absolute -mb-9 h-7 w-full transform scale-75 bg-[#252931] rounded-[100%] -bottom-2 z-0 blur transition ease-in-out duration-300 ${
+    isHovered ? "blur-md" : ""
+  }`;
+  const shadowClassName = `${selectedCup ? selectedShadow : sideShadow}`;
 
   return (
     <div className={containerClassName}>
